cfca93caaed5fecd87e3ec8397d03c614b37291a
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-04-09 23:04:52 +0530

    more patterns

--- src/Patterns.java
+++ src/Patterns.java
@@ -41,13 +41,26 @@ public class Patterns {
 //
 
 
-        int  n = 5;
-        for (int i = 1;i<=n;i++){
-            //stars
-            for (int j = 1; j<=n-i+1;j++){
+//        int  n = 5;
+//        for (int i = 1;i<=n;i++){
+//            //stars
+//            for (int j = 1; j<=n-i+1;j++){
+//                System.out.print("* ");
+//            }
+//
+//            System.out.println();
+//        }
+
+
+        int n=5;
+        for (int  i = 1; i<=5;i++){
+            //spaces
+            for (int j = 1; j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j= 1; j<=2*i-1;j++){
                 System.out.print("* ");
             }
-
             System.out.println();
         }
     }
