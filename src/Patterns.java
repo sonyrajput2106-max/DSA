94376e24cc583fb75aaa4c50d463c3cf3ce590b2
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-04-09 22:57:51 +0530

    learning patterns

---
+++ src/Patterns.java
@@ -0,0 +1,54 @@
+public class Patterns {
+    static void main(){
+
+
+//        the core formula
+//        for(row =1 to n)
+//        for(col=1 to n or variable)
+//        print
+
+
+//        int n=3;
+//        for (int i =1; i<=n;i++){
+//            for (int  j=1; j<=5;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println("");
+//        }
+//int n=5;
+//         for (int i=1; i<=n;i++ ){
+//             for (int j = 1; j<=i;j++){
+//                 System.out.print("* ");
+//             }
+//             System.out.println("");
+//         }
+
+
+//        int n= 5;
+//        for (int row=1; row<=5;row++){
+//            //for eaxh row -> spaces, stars
+//
+//            //spaces
+//            for (int col=1; col<=n-row;col++){
+//                System.out.print(" ");
+//            }
+//            //stars
+//            for (int col=1;col<=n;col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+
+
+        int  n = 5;
+        for (int i = 1;i<=n;i++){
+            //stars
+            for (int j = 1; j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
