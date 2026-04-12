b5cd94073fb16cc2346f0a1045465119ea52b08f
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-04-13 00:42:07 +0530

    min max in array

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,21 +1,51 @@
-import java.util.Scanner;
-
 public class Arrays {
     static void main() {
 
-        int arr[] = new int[5];
-        Scanner sc = new Scanner(System.in);
+        //find maximum value from the array
+        int arr[] = {3, 2, -5, 21, 15};
         int n = arr.length;
-        for (int i = 0; i <= n - 1; i++) {
-            System.out.println("provide input for index:" + i);
-            arr[i] = sc.nextInt();
-        }
+        int maxValue = arr[0];
 
-        //print
-        System.out.println("You array contains:");
-        for (int val : arr) {
-            System.out.println(val);
+        //compare maxValue with array elements
+        for (int i = 0; i <= n - 1; i++) {
+            if (arr[i] > maxValue) {
+                //update maxValue
+                maxValue = arr[i];
+            }
         }
+        System.out.println(maxValue);
+
+
+        //sum of elements of the array
+//        int arr[] = {10, 20, 30, 40, 50};
+//        int sum = 0;
+//        int n = arr.length;
+//        for (int i = 0; i <= n - 1; i++) {
+//            int value = arr[i];
+//            sum = sum + value;
+//        }
+//        System.out.println(sum);
+
+//        int arr[] = {12, 14, 1245};
+//
+//        int n = arr.length;
+//        for (int i = 0; i <= n - 1; i++) {
+//            System.out.println(arr[i]);
+//        }
+
+//        int arr[] = new int[5];
+//        Scanner sc = new Scanner(System.in);
+//        int n = arr.length;
+//        for (int i = 0; i <= n - 1; i++) {
+//            System.out.println("provide input for index:" + i);
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        //print
+//        System.out.println("You array contains:");
+//        for (int val : arr) {
+//            System.out.println(val);
+//        }
         //declaration
 //        int arr[];
 //
