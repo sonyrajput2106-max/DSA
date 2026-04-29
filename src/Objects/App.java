b92244ecb5b9d05d03dbfd02d19d467578515e39
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-04-29 16:36:53 +0530

    encapsulation in java

---
+++ src/Objects/App.java
@@ -0,0 +1,63 @@
+package Objects;
+
+public class App{
+public static void main(String[] args) {
+//    Objects A = new Objects();
+//    A.id = 13;
+//    A.age = 34;
+//    A.name ="sona";
+//    A.nos =4;
+//    System.out.println(A.name);
+//    System.out.println(A.id);
+//    System.out.println(A.age);
+//    System.out.println(A.nos);
+//
+//    A.bunk();
+//    A.study();
+//    A.sleep();
+
+    //Parameterized constructor
+//    Objects A = new Objects(1,12, "sona", 3);
+//    System.out.println(A.name);
+//    System.out.println(A.id);
+//    System.out.println(A.age);
+//    System.out.println(A.nos);
+//
+//    A.bunk();
+//    A.study();
+//    A.sleep();
+//
+//    Objects B= new Objects(2,14, "nona", 5);
+//    Objects C = new Objects(3,12, "tona", 7);
+//    Objects D = new Objects(4,16, "mona", 3);
+
+
+    //Copy constructor
+//    Objects B = new Objects(A);
+//    System.out.println(B.name);
+//    System.out.println(B.id);
+//    System.out.println(B.age);
+//    System.out.println(B.nos);
+//
+//    B.sleep();
+
+
+    Students A = new Students(1, 12, "rahul", 4, "tina");
+//    System.out.println(A.name);
+//    System.out.println(A.id);
+//    System.out.println(A.age);
+//    System.out.println(A.nos);
+//    System.out.println(A.getGf());
+
+//    A.bunk();
+//    A.study();
+//    A.sleep();
+//    A.gfChatting();
+
+    System.out.println(A.getAge());
+
+    A.setAge(45);
+    System.out.println(A.getAge());
+
+}
+}
--- src/Objects/Students.java
+++ src/Objects/Students.java
@@ -1,26 +1,45 @@
-public class Objects {
+package Objects;
+
+public class Students {
     //Attributes
     public int id;
-    public int age;
+    private int age;
     public String name;
     public int nos;
+    private String gf;
+
+    public String getGf(){
+        return this.gf;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int a){
+        //extra layer of authentication
+        if (age <100){
+        this.age =a;}
+        return;
+    }
 
     //Default constructor
-    public Objects() {
+    public Students() {
         System.out.println("student default constructor called ");
     }
 
     //Parameterized constructor
-    public Objects(int id , int age , String name , int nos) {
+    public Students(int id , int age , String name , int nos , String gf) {
         System.out.println("student Parameterized  constructor called ");
         this.id= id;
         this.age = age;
         this.name = name;
         this.nos = nos;
+        this.gf=gf;
     }
 
     //Copy constructor
-    public Objects(Objects srcobj) {
+    public Students(Students srcobj) {
         System.out.println("student Parameterized  constructor called ");
         this.id= srcobj.id;
         this.age =srcobj.age;
@@ -30,15 +49,19 @@ public class Objects {
 
     //,Methods/Behaviour
     public void study() {
-        System.out.println(name + "studying");
+        System.out.println(name + " studying");
     }
 
     public void sleep() {
-        System.out.println(name + "sleeping");
+        System.out.println(name + " sleeping");
     }
 
     public void bunk() {
-        System.out.println(name + "bunking");
+        System.out.println(name + " bunking");
+    }
+
+    private void gfChatting(){
+        System.out.println(name +" chatting");
     }
 }
 
