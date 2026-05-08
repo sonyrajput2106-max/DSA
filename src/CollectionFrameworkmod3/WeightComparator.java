3d9315e12ed3156751a1bbf0aeaa12940081fb0c
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-05-08 23:00:08 +0530

    weight comparator

--- src/CollectionFrameworkmod3/WeightComparator.java
+++ src/CollectionFrameworkmod3/WeightComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class WeightComparator implements Comparator<Student> {
 
 
+    //this is a comment
     @Override
     public int compare(Student o1, Student o2) {
         return o1.weight - o2.weight;
