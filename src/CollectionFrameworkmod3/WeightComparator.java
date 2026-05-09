66f76c8d17c6d200d09897abb548a91efc18ae14
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-05-09 23:40:24 +0530

    weight comparator using collection framework

--- src/CollectionFrameworkmod3/WeightComparator.java
+++ src/CollectionFrameworkmod3/WeightComparator.java
@@ -6,6 +6,7 @@ public class WeightComparator implements Comparator<Student> {
 
 
     //this is a comment
+    //hhh
     @Override
     public int compare(Student o1, Student o2) {
         return o1.weight - o2.weight;
