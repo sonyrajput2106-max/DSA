9e96347cb93b48c0bbe02a39db01d19cdd43f18d
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-05-13 23:18:08 +0530

    bitwise operators

---
+++ src/BitwiseOperators/BitwiseOperators.java
@@ -0,0 +1,19 @@
+package BitwiseOperators;
+
+public class BitwiseOperators {
+    static void main() {
+
+//        int a = 5;
+//        int b = 6;
+//        System.out.println(a|b);
+//        System.out.println(a & b);
+//        System.out.println(a ^ b);
+//        System.out.println(~b); // by 2's compliment
+//        System.out.println( a<< 1); //  left shift ->  multiplied by 2
+
+
+        int n = 5;
+
+   }
+
+}
