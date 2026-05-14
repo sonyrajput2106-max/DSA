cfb51e77f9239b3ddf42a9928ca386a54a36a1a2
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-05-14 23:58:21 +0530

    linear search on array

--- src/Lecture27/ArrayProblems.java
+++ src/Lecture27/ArrayProblems.java
@@ -12,8 +12,45 @@ public class ArrayProblems {
         double avg = sum/size;
         return avg;
     }
+
+
+    //Multiplying elents in the array by ten
+    static int[] mulElements(int[] arr){
+    int [] newArray = new int[arr.length];
+
+    for (int i =0; i<arr.length; i++){
+    newArray[i]=arr[i]*10;
+    }
+    return newArray;
+    }
+
+
+    //Searching for an element in the array
+    static boolean findElement(int[] arr){
+        int target =7;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] == target){
+                return true;
+            }
+        }
+        //all the array traversed
+        return false;
+    }
+
+
     static void main() {
-        int[] arr ={2,4,5,2,5};
-        System.out.println(getAverage(arr));
+//        int[] arr ={2,4,5,2,5};
+//        System.out.println(getAverage(arr));
+
+//        int [] arr ={1,2,3,4,5};
+//        int[] result = mulElements(arr);
+//        for (int i : result){
+//            System.out.println(i+" ");
+//        }
+
+        int arr[] ={2,4,3,6,7};
+        boolean ans = findElement(arr);
+        System.out.println(ans);
+
     }
 }
