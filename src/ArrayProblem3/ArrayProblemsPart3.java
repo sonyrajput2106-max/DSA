94f439c83145c62607d6ec18b9f8098f823ed008
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-05-27 00:30:14 +0530

    sorting 0's 1's nd 2's

--- src/ArrayProblem3/ArrayProblemsPart3.java
+++ src/ArrayProblem3/ArrayProblemsPart3.java
@@ -1,4 +1,5 @@
 package ArrayProblem3;
+import java.util.Arrays;
 
 public class ArrayProblemsPart3 {
 
@@ -48,6 +49,42 @@ int xorSum = 0;
         }
         return xorSum2;
     }
+
+    //sorting 0's ' 1's and 2's
+    static int[] sort(int[] nums){
+        int n = nums.length;
+        int low=0;
+        int mid =0;
+        int high = n-1;
+
+        while(mid <= high){
+            if (nums[mid] ==0){
+//                swap  mid with low nd increment low nd mid
+                int temp = nums[low];
+                nums[low]=nums[mid];
+                nums[mid]= temp;
+
+                //increment low
+                low++;
+                mid++;
+
+            }
+
+            else if (nums[mid]==1){
+                //mid ko age bdhao
+                mid++;
+            }
+            else if(nums[mid]==2){
+                //swap with high
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                //nd decrement high
+                high --;
+            }
+        }
+    return nums;
+    }
     static void main() {
 //        int[] arr = {1,0,1,0,0,1,1};
 //        System.out.println(sortZeroOnes(arr));
@@ -55,11 +92,18 @@ int xorSum = 0;
 //            System.out.print(i + " ");
 //        }
 
-        int [] arr ={0,4,3,1,2};
-        System.out.println(findMissingNumber(arr));
+//        int [] arr ={0,4,3,1,2};
+//        System.out.println(findMissingNumber(arr));
+//
+//
+//        int[] arr1 ={ 2,3,4,3,2,1};
+//        System.out.println(uniqueElement(arr1));
 
+        int[] arr = {2,0,1,2,1,0};
 
-        int[] arr1 ={ 2,3,4,3,2,1};
-        System.out.println(uniqueElement(arr1));
+        int[] ans = sort(arr);
+
+//        System.out.println(ans);
+        System.out.println(Arrays.toString(ans));
     }
 }
