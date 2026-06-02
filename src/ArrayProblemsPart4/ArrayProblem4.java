28be56c0c7e5a5dd519565b1117de26e3b503d27
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-02 22:28:08 +0530

    check if array is sorted

--- src/ArrayProblemsPart4/ArrayProblem4.java
+++ src/ArrayProblemsPart4/ArrayProblem4.java
@@ -203,6 +203,20 @@ return -1;
 //
 //System.out.println(secondLargest);
 
+    //check if array is sorted
+//    int[] arr = {1, 2, 3, 4, 5};
+//
+//    boolean sorted = true;
+//
+//for(int i = 0; i < arr.length - 1; i++) {
+//        if(arr[i] > arr[i + 1]) {
+//            sorted = false;
+//            break;
+//        }
+//    }
+//
+//System.out.println(sorted);
+
     static void main() {
 //        int[] arr ={2,5,7,3,1};
 //        System.out.println(Arrays.toString(twoSum(arr)));
