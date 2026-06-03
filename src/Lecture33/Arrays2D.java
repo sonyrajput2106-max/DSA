c802bd72fc1b768c290c222817da675fe2694c9c
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-03 23:49:16 +0530

    sum of each row in 2d array

--- src/Lecture33/Arrays2D.java
+++ src/Lecture33/Arrays2D.java
@@ -1,4 +1,35 @@
 package Lecture33;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arrays2D {
+
+    static  List<Integer> rowSum(int [][]  arr){
+
+        List<Integer> result = new ArrayList<>();
+
+        int m = arr.length;
+        int n = arr.length;
+
+        //traversal
+        for (int row=0; row<m ; row++ ){
+             int sum =0;
+
+             for (int col=0; col< n;col++){
+                 int value = arr[row][col];
+                 sum = sum + value;
+
+             }
+
+             result.add(sum);
+        }
+        return new ArrayList<>();
+    }
+    static void main() {
+        int  arr[][] = {{1,2,3}, {4,5,6},{7,8,9}};
+        System.out.println(rowSum(arr));
+
+
+
+    }
 }
