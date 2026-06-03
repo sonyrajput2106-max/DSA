caa3ee42c9b8a8cd49c6dae3df3ff683d4a279e3
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-04 00:32:01 +0530

    wave print a matrix

--- src/Lecture33/Arrays2D.java
+++ src/Lecture33/Arrays2D.java
@@ -28,15 +28,38 @@ public class Arrays2D {
 
 
         //for column wise sum
-        for (int col=0; col<n;col++){
-            int sum = 0;
-            for ( int row=0; row<m;row++){
-                int value = arr[col][row];
-                sum = sum + value;
+//        for (int col=0; col<n;col++){
+//            int sum = 0;
+//            for ( int row=0; row<m;row++){
+//                int value = arr[col][row];
+//                sum = sum + value;
+//            }
+//            result.add(sum);
+//        }
+//return result;
+
+
+        //wave print a matrix
+        // let's move column wise
+        for (int col =0; col<n; col++){
+//har ek column index ko check kro even/ odd k liye
+            if ((col & 1)==1){
+                //odd
+                //bottom to top
+                for (int row = m-1; row>=0;row--){
+                    result.add(arr[row][col]);
+
+                }
             }
-            result.add(sum);
+            else{
+                //even
+                //top to bottom
+                for (int row =0; row<m; row++){
+                    result.add(arr[row][col]);
+                }
+            }
         }
-return result;
+        return result;
     }
     static void main() {
         int  arr[][] = {{1,2,3}, {4,5,6},{7,8,9}};
