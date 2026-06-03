331f9b8c7cc023a4fd28dc4104d88d83614dcbae
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-04 00:01:06 +0530

    sum of 2d array column wise

--- src/Lecture33/Arrays2D.java
+++ src/Lecture33/Arrays2D.java
@@ -9,21 +9,34 @@ public class Arrays2D {
         List<Integer> result = new ArrayList<>();
 
         int m = arr.length;
-        int n = arr.length;
+        int n = arr[0].length;
 
         //traversal
-        for (int row=0; row<m ; row++ ){
-             int sum =0;
+//        for (int row=0; row<m ; row++ ){
+//             int sum =0;
+//
+//             for (int col=0; col< n;col++){
+//                 int value = arr[row][col];
+//                 sum = sum + value;
+//
+//             }
+//
+//             result.add(sum);
+//        }
+//        return result;
+//
 
-             for (int col=0; col< n;col++){
-                 int value = arr[row][col];
-                 sum = sum + value;
 
-             }
-
-             result.add(sum);
+        //for column wise sum
+        for (int col=0; col<n;col++){
+            int sum = 0;
+            for ( int row=0; row<m;row++){
+                int value = arr[col][row];
+                sum = sum + value;
+            }
+            result.add(sum);
         }
-        return new ArrayList<>();
+return result;
     }
     static void main() {
         int  arr[][] = {{1,2,3}, {4,5,6},{7,8,9}};
