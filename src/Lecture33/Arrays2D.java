73e5f4bfa302c8e58673f8921e257c12a1e6dec3
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-04 16:34:25 +0530

    spiral printing an array

--- src/Lecture33/Arrays2D.java
+++ src/Lecture33/Arrays2D.java
@@ -65,37 +65,130 @@ public class Arrays2D {
     }
 
     //transpose of a matrix
-static int [][] transpose(int[][] matrix){
-        if (matrix ==null || matrix.length ==0){
-            return new int[0][0];
+//static int [][] transpose(int[][] matrix){
+//        if (matrix ==null || matrix.length ==0){
+//            return new int[0][0];
+//        }
+//
+//        //for original array
+//    int totalRows = matrix.length;
+//        int totalCols = matrix[0].length;
+//
+//        //for new array
+//    int newTotalRows = totalCols;
+//    int newTotalCols = totalRows;
+//    int ans[][] = new int [newTotalCols][newTotalRows];
+//
+//    //actual logic
+//    for (int row=0; row<totalRows;row++){
+//        for (int col =0; col<totalCols;col++) {
+//            ans[col][row] = matrix[row][col];
+//        }
+//    }
+//    return ans;
+//}
+
+// rotate a matrix
+    static void rotate90Clockwise(int [][] matrix,int N){
+        //step -1 transpose of matrix
+        // swap matrix[i[j], matrix[j][i]
+        for (int i=0;i<N ; i++){
+            for (int j = i+1; j<N; j++){
+                //swap
+             int temp = matrix[i][j];
+             matrix[i][j] = matrix[j][i];
+             matrix[j][i] = temp;
+            }
         }
 
-        //for original array
-    int totalRows = matrix.length;
-        int totalCols = matrix[0].length;
+        //step -2 reverse all rows
+        for(int row =0; row<N; row++) {
+            int startCol = 0;
+            int endCol = N - 1;
+            while (startCol <= endCol) {
+                //swap matrix[row] [startCol] , matrix[row][endCol]}
+                int temp = matrix[row][startCol];
+                matrix[row][startCol] = matrix[row][endCol];
+                matrix[row][endCol] = temp;
 
-        //for new array
-    int newTotalRows = totalCols;
-    int newTotalCols = totalRows;
-    int ans[][] = new int [newTotalRows][newTotalCols];
+                startCol++;
+                endCol--;
 
-    //actual logic
-    for (int row=0; row<totalRows;row++){
-        for (int col =0; col<totalCols;col++) {
-            ans[col][row] = matrix[row][col];
+            }
         }
+
     }
-    return ans;
-}
 
-    static void main() {
+    //spiral print a matrix
+    static List<Integer> spiralOrder(int[][] matrix2){
+        int m = matrix2.length;
+        int  n = matrix2[0].length;
+
+        List<Integer> result = new ArrayList<>();
+        int startingRow =0;
+        int endingRow = m-1;
+        int startingCol = 0;
+        int endingCol = n-1;
+
+        while(startingRow<= endingRow && startingCol <= endingCol){
+            //row wise left to right -> startingRow print krni h , from startingcol to endingcol
+            for (int col = startingCol ; col <=endingCol;col++){
+                result.add(matrix2[startingRow][col]);
+            }
+            startingRow++;
+
+            //col wise top to bottom -> ending col pring krna h , startingrow to ending row
+            for ( int row = startingRow; row<=endingRow;row++){
+                result.add(matrix2[row][endingCol]);
+            }
+            endingCol--;
+
+            //row wise right to left -> ending row print krni h -> endingcol to starting col
+            if (startingRow<= endingRow) {
+                for (int col = endingCol; col <= startingCol; col--) {
+                    result.add(matrix2[endingRow][col]);
+                }
+                endingRow--;
+            }
+
+            //col wise bottom to top -> startCol print krna h,endingrow to starting row
+            if (startingCol <= endingCol ){
+            for (int row=endingRow; row<= startingRow;row--){
+                result.add(matrix2[row][startingCol]);
+            }
+            startingCol++;
+            }
+        }
+        return result;
+    }
+
+
+
+
+   static void main() {
 //        int  arr[][] = {{1,2,3}, {4,5,6},{7,8,9}};
 //        System.out.println(rowSum(arr));
 
         //transpose
-        int matrix[][] ={{1,2,3}, {4,5,6}};
-        System.out.println(Arrays.deepToString(transpose(matrix)));
+//        int matrix[][] ={{1,2,3}, {4,5,6}};
+//        System.out.println(Arrays.deepToString(transpose(matrix)));
 
+            int matrix[][] = {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}
+            };
+
+            rotate90Clockwise(matrix, 3);
+
+            for (int[] row : matrix) {
+                System.out.println(java.util.Arrays.toString(row));
+            }
+
+
+            //spiral print
+       int[][] matrix2 = {{1,2,3},{4,5,6},{7,8,9}};
+       System.out.println(spiralOrder(matrix2));
 
 
     }
