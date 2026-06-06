fafbb5d090ac4fd2b79726b1f78e3ec882a9e78a
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-06 16:16:14 +0530

    Upper Bound

--- src/BinarySearch/BinarySearchImp.java
+++ src/BinarySearch/BinarySearchImp.java
@@ -53,13 +53,41 @@ public class BinarySearchImp {
 return ans;
     }
 
+
+    //Upper Bound
+    static int getUpperBound(int arr[], int target){
+        int n = arr.length;
+        int s=0;
+        int e=n-1;
+        int ans = -1;
+
+        while(s<=e){
+            int mid = s+ (e-s)/2;
+
+            if(arr[mid]<=  target){
+                // move right
+                s= mid + 1;
+
+            }
+            else{
+                //arr[mid] > target
+                //store ans
+                ans = mid;
+                //move left
+                e = mid -1;
+            }
+        }
+        return ans;
+    }
+
     static void main() {
 //         int arr[] ={-1,0,3,5,9,12};
 //        System.out.println(binarySearch(arr));
 
-        int target=30;
+        int target=35;
        int arr[]={10,20,30,30,30,30,30,40,50};
-        int ans = getLowerBound(arr , target);
+//        int ans = getLowerBound(arr , target);
+        int ans = getUpperBound(arr, target);
        System.out.println(ans);
     }
 }
