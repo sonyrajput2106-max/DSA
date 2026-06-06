bd5eb3429a492dcd68ed544262ec346f42f38301
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-06 16:08:24 +0530

    lower bound

--- src/BinarySearch/BinarySearchImp.java
+++ src/BinarySearch/BinarySearchImp.java
@@ -8,7 +8,7 @@ public class BinarySearchImp {
         int n = arr.length;
         int start =0;
         int end = n-1;
-        int mid = (start+end)/2;
+        int mid = (start+end)/2; // mid = start + (end-start)/2
 
         while(start <= end){
             //compare target with midValue
@@ -28,8 +28,38 @@ public class BinarySearchImp {
         return -1;
     }
 
+    //Lower bound
+    static int getLowerBound(int arr[], int target){
+
+        int n = arr.length;
+        int s=0;
+        int e=n-1;
+        int ans = -1;
+
+        while(s<=e){
+            int mid = s+(e-s)/2;
+
+            if (arr[mid]>= target){
+                //store ans
+                ans = mid;
+                //move to left
+                e= mid-1;
+            }
+            else{
+                //move to right
+                s = mid +1;
+            }
+        }
+return ans;
+    }
+
     static void main() {
-         int arr[] ={-1,0,3,5,9,12};
-        System.out.println(binarySearch(arr));
+//         int arr[] ={-1,0,3,5,9,12};
+//        System.out.println(binarySearch(arr));
+
+        int target=30;
+       int arr[]={10,20,30,30,30,30,30,40,50};
+        int ans = getLowerBound(arr , target);
+       System.out.println(ans);
     }
 }
