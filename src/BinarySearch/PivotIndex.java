5d241d4dda657c9c1a2b42a48eb623eb29c0a0c5
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-06-08 15:04:42 +0530

    search in rotated sorted array

--- src/BinarySearch/PivotIndex.java
+++ src/BinarySearch/PivotIndex.java
@@ -28,9 +28,46 @@ return ans;
 
     }
 
-    static void main() {
-int arr[ ] = { 4,5,6,0,1,2};
-        System.out.println(findPivotIndex(arr));
+    static int searchRotatedArr(int arr[], int target){
+      int start =0;
+      int end = arr.length-1;
 
+      while(start <= end){
+          int mid = start +(end - start)/2;
+
+          if (arr[mid]==target){
+              return mid;
+          }
+
+          //left half is sorted
+          if (arr[start]<= arr[mid]){
+              if (target>= arr[start] && target < arr[mid]){
+                  end = mid-1;
+              }
+              else{
+                  start = mid +1;
+              }
+          }
+
+          //right half is sorted
+          else{
+              if (target > arr[mid] && target <= arr[end]){
+                  start = mid +1;
+
+              }
+              else{
+                  end = mid -1;
+              }
+          }
+      }
+        return -1;
+    }
+
+    static void main() {
+int arr[] = { 4,5,6,0,1,2};
+//        System.out.println(findPivotIndex(arr));
+
+int target = 2;
+        System.out.println(searchRotatedArr(arr, target));
     }
 }
