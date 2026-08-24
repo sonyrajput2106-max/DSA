92838e70a8ae8d59f9f7b493f601c22bd3ea605e
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-08-24 23:24:31 +0530

    more basic ques on recursion

--- src/Recursion/BasicQues.java
+++ src/Recursion/BasicQues.java
@@ -24,13 +24,97 @@ public class BasicQues {
         printNum(n,count+1);
     }
 
+    static void printArr(int[] arr, int i){
+        //base case
+        if (i>=arr.length){
+            return;
+        }
+
+        System.out.println(arr[i]);
+
+        printArr(arr,i+1);
+    }
+
+    static void printMaxElement(int arr[] , int i,int maxi){
+        //base case
+        if (i>=arr.length){
+            System.out.println("max value:" + maxi);
+            return ;
+        }
+
+
+        if (arr[i]>maxi){
+            maxi = arr[i];
+        }
+
+        printMaxElement(arr,i+1 , maxi);
+    }
+
+
+    static int findTarget(int arr[] , int i , int target){
+        //base case
+        if (i>=arr.length){
+            return -1;
+        }
+
+        if (arr[i]==target){
+            return i;
+        }
+
+        int ans = findTarget(arr,i+1, target);
+        return ans;
+    }
+
+
+    static int findCount(int arr[], int i , int target , int count){
+        //base case
+        if (i>=arr.length){
+            System.out.println(count);
+            return -1;
+        }
+
+        if (arr[i]==target){
+            count ++;
+        }
+
+        int ans = findCount(arr, i , target , count);
+        return ans;
+    }
+
+    static void printDigits(int n){
+        //base case
+        if (n==0){
+            return;
+        }
+        int digit = n%10;
+//        System.out.println(digit);
+        n =n/10;
+
+        printDigits(n);
+
+        System.out.println(digit);
+    }
+
     static void main() {
 //        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //
 //        printMyName(n);
 
-        printNum(5,1);
+//        printNum(5,1);
 
+        int i =0;
+        int arr[]={1,5,6,8,6,2,6};
+//        int maxi = Integer.MIN_VALUE;
+//        printMaxElement(arr, i , maxi);
+
+
+        int target =6;
+//        findTarget(arr,i,target);
+//        printArr(arr, i);
+
+        int count =0;
+//        findCount(arr, i,target, count);
+    printDigits(137);
     }
 }
