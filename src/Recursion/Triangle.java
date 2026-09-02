c89e5013878b08cad3051ec986355798b592537c
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-09-02 23:38:42 +0530

    Triangle problem

--- src/Recursion/Triangle.java
+++ src/Recursion/Triangle.java
@@ -6,42 +6,26 @@ import java.util.List;
 
 public class Triangle {
 
-    public static int solve(List<List<Integer>> triangle,
-                            int row, int col, int[][] dp) {
-
-        // Base case
-        if (row == triangle.size() - 1) {
-            return triangle.get(row).get(col);
+    public static int solve(List<List<Integer>> triangle, int rowIndex , int colIndex) {
+        //base case
+        if (rowIndex==triangle.size()-1){
+            return triangle.get(rowIndex).get(colIndex);
         }
 
-        // Already calculated
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
+        int downAns = solve(triangle, rowIndex+1,colIndex);
+        int diagonalAns = solve(triangle,rowIndex+1,colIndex+1);
+        int finalAns = triangle.get(rowIndex).get(colIndex)+Math.min(downAns,diagonalAns);
+        return finalAns;
 
-        // Go down
-        int down = solve(triangle, row + 1, col, dp);
 
-        // Go diagonally
-        int diagonal = solve(triangle, row + 1, col + 1, dp);
-
-        // Minimum path
-        dp[row][col] = triangle.get(row).get(col)
-                + Math.min(down, diagonal);
-
-        return dp[row][col];
     }
 
     public static int minimumTotal(List<List<Integer>> triangle) {
 
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-        return solve(triangle, 0, 0, dp);
+      int rowIndex =0;
+      int colIndex =0;
+      int ans = solve(triangle,rowIndex,colIndex);
+      return ans;
     }
 
     public static void main(String[] args) {
